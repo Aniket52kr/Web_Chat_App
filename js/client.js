db4b5e2a22d70a9e22634e38defd9c21d9ad1be9
Aniket52kr <aniketbawankar2021@gmail.com>
2024-05-31 23:04:03 +0530

    files added

--- js/client.js
+++ js/client.js
@@ -1,3 +1,63 @@
+// const socket = io('http://localhost:8000');
+
+// socket.on('connect', () => {
+//     console.log('Connected to server');
+// });
+
+// socket.on('disconnect', () => {
+//     console.log('Disconnected from server');
+// });
+
+// const form = document.getElementById('send-container');
+// const messageInput = document.getElementById('msgInput');
+// const messageContainer = document.querySelector('.wrapper');
+// var audio = new Audio('assests_notify.mp3');
+
+// const append = (message, position) => {
+//     const messageElement = document.createElement('div');
+//     messageElement.innerText = message;
+//     messageElement.classList.add('msg');
+//     messageElement.classList.add(position);
+//     messageContainer.append(messageElement);
+//     if (position == 'left') {
+//         audio.play();
+//     }
+// };
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     const message = messageInput.value;
+//     console.log('Sending message:', message);
+//     append(`You: ${message}`, 'right');
+//     socket.emit('send', message);
+//     messageInput.value = '';
+// });
+
+// const name = prompt("Enter your name to join");
+// console.log('User name:', name);
+// socket.emit('new-user-joined', name);
+
+// socket.on('user-joined', (name) => {
+//     console.log('User joined:', name);
+//     append(`${name} joined the chat`, 'right');
+// });
+
+// socket.on('receive', (data) => {
+//     console.log('Received message:', data);
+//     append(`${data.name}: ${data.message}`, 'left');
+// });
+
+// socket.on('left', (name) => {
+//     console.log('User left:', name);
+//     append(`${name} left the chat!`, 'left');
+// });
+
+
+
+
+
+
+
 const socket = io('http://localhost:8000');
 
 socket.on('connect', () => {
@@ -11,7 +71,7 @@ socket.on('disconnect', () => {
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('msgInput');
 const messageContainer = document.querySelector('.wrapper');
-var audio = new Audio('assests_notify.mp3');
+var audio = new Audio('assets_notify.mp3');
 
 const append = (message, position) => {
     const messageElement = document.createElement('div');
@@ -19,7 +79,7 @@ const append = (message, position) => {
     messageElement.classList.add('msg');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
-    if (position == 'left') {
+    if (position === 'left') {
         audio.play();
     }
 };
